5e55de9d51784800f3c763ff1bf64d83b35a11f2
Hongjian Zhu <zhu.life@gmail.com>
2025-01-05 23:09:12 +0800

    wip

--- app.cc
+++ app.cc
@@ -1229,13 +1229,13 @@ void app_update(App *app, InputSystemState *input_system_state) {
     mouse_positions[frame_index] = {};
     is_mouse_start_up[frame_index] = {};
   }
-  // GeometryConfig config{};
-  // generate_sector_geometry_config(app->gizmo.rotation_plane_normal, app->gizmo.rotation_start_pos, app->gizmo.rotation_end_pos, app->gizmo.rotation_clock_dir, 64, &config);
-  // Geometry *geometry = new Geometry();
-  // create_geometry_from_config(&app->mesh_system_state, app->vk_context, &config, geometry);
-  // dispose_geometry_config(&config);
-  // rotation_sector_geometries[app->frame_count % FRAMES_IN_FLIGHT] = {.geometry = geometry, .frame_count = app->frame_count};
-  // log_debug("frame %d frame index %d, rotation sector geometry created %p index handle %p", app->frame_count, app->frame_count % FRAMES_IN_FLIGHT, geometry, geometry->meshes.front().index_buffer->handle);
+  GeometryConfig config{};
+  generate_sector_geometry_config(app->gizmo.rotation_plane_normal, app->gizmo.rotation_start_pos, app->gizmo.rotation_end_pos, app->gizmo.rotation_clock_dir, 64, &config);
+  Geometry *geometry = new Geometry();
+  create_geometry_from_config(&app->mesh_system_state, app->vk_context, &config, geometry);
+  dispose_geometry_config(&config);
+  rotation_sector_geometries[app->frame_count % FRAMES_IN_FLIGHT] = {.geometry = geometry, .frame_count = app->frame_count};
+  log_debug("frame %d frame index %d, rotation sector geometry created %p index handle %p", app->frame_count, app->frame_count % FRAMES_IN_FLIGHT, geometry, geometry->meshes.front().index_buffer->handle);
 
   update_scene(app);
   RenderFrame *frame = &app->frames[frame_index];
